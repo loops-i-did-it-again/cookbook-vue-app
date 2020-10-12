042880a2a2c54817b09983b2c9671bba80e5609c
dani <dani.zaghian@gmail.com>
2020-10-12 14:02:57 -0700

    adds recipes create action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="home">
-    <h1>Recipes</h1>
+    <div>
+      <h2>New Recipe</h2>
+      <button v-on:click="createRecipe()">Create</button>
+    </div>
+
     <div v-for="recipe in recipes">
       <h2>Title: {{ recipe.title }}</h2>
+      <img :src="recipe.image_url" alt="">
       <p>Ingredients: {{ recipe.ingredients }}</p>
       <p>Directions: {{ recipe.directions }}</p>
       <p>Prep Time: {{ recipe.prep_time }}</p>
@@ -11,6 +16,9 @@
 </template>
 
 <style>
+img {
+  width: 250px;
+}
 </style>
 
 <script>
@@ -30,6 +38,23 @@ export default {
       axios.get("/api/recipes").then(response => {
         console.log(response.data);
         this.recipes = response.data;
+      });
+    },
+    createRecipe: function () {
+      var params = {
+        title: "Example 2 Title",
+        image_url: "example 2.png",
+        ingredients: "Example 2 ingredients",
+        directions: "Example 2 directions",
+        prep_time: 10
+      };
+
+      axios.post("/api/recipes", params).then(response => {
+        console.log("Success", response.data);
+        this.recipes.push(response.data);
+      })
+      .catch(error => {
+        console.log(error.response.data.errors);
       });
     }
   }
