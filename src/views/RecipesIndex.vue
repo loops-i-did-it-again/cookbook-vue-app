37be7a690599eb76db0e3a391506684df61dae52
dani <dani.zaghian@gmail.com>
2020-10-19 13:59:47 -0700

    updates syntax

--- src/views/RecipesIndex.vue
+++ src/views/RecipesIndex.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="recipes-index">
-
     <div class="card-columns">
       <div class="card" v-for="recipe in recipes">
         <router-link :to="`/recipes/${recipe.id}`">
-          <img :src="recipe.image_url" class="card-img-top" alt="">
+          <img :src="recipe.image_url" class="card-img-top" alt="" />
         </router-link>
         <div class="card-body">
           <h5 class="card-title">{{ recipe.title }}</h5>
           <p class="card-text">Prep Time: {{ recipe.prep_time }}</p>
-          <p class="card-text"><small class="text-muted">Created {{ recipe.created_at }}</small></p>
+          <p class="card-text">
+            <small class="text-muted">Created {{ recipe.created_at }}</small>
+          </p>
         </div>
       </div>
     </div>
-
   </div>
 </template>
 
@@ -23,16 +23,15 @@ import axios from "axios";
 export default {
   data: function() {
     return {
-      recipes: []
+      recipes: [],
     };
   },
   created: function() {
-    axios
-      .get("/api/recipes").then(response => {
-        console.log(response.data);
-        this.recipes = response.data;
-      });
+    axios.get("/api/recipes").then((response) => {
+      console.log(response.data);
+      this.recipes = response.data;
+    });
   },
-  methods: {}
+  methods: {},
 };
 </script>
