4d6a87d344cea2a32f56a96c23b118dab3181a17
dani <dani.zaghian@gmail.com>
2020-11-03 13:31:30 -0800

    adds image uploading

--- src/views/RecipesNew.vue
+++ src/views/RecipesNew.vue
@@ -6,26 +6,31 @@
         <li class="text-danger" v-for="error in errors">{{ error }}</li>
       </ul>
       <div class="form-group">
-        <label>Title:</label> 
-        <input type="text" class="form-control" v-model="title">
+        <label>Title:</label>
+        <input type="text" class="form-control" v-model="title" />
       </div>
       <div class="form-group">
         <label>Ingredients:</label>
-        <input type="text" class="form-control" v-model="ingredients">
+        <input type="text" class="form-control" v-model="ingredients" />
       </div>
       <div class="form-group">
         <label>Directions:</label>
-        <input type="text" class="form-control" v-model="directions">
+        <input type="text" class="form-control" v-model="directions" />
       </div>
       <div class="form-group">
         <label>Image Url:</label>
-        <input type="text" class="form-control" v-model="imageUrl">
+        <input
+          type="file"
+          class="form-control"
+          v-on:change="setFile($event)"
+          ref="fileInput"
+        />
       </div>
       <div class="form-group">
         <label>Prep Time:</label>
-        <input type="number" class="form-control" v-model="prepTime">
+        <input type="number" class="form-control" v-model="prepTime" />
       </div>
-      <input type="submit" class="btn btn-primary" value="Submit">
+      <input type="submit" class="btn btn-primary" value="Submit" />
     </form>
   </div>
 </template>
@@ -39,30 +44,34 @@ export default {
       title: "",
       ingredients: "",
       directions: "",
-      imageUrl: "",
       prepTime: "",
-      errors: []
+      image: "",
+      errors: [],
     };
   },
   methods: {
+    setFile: function(event) {
+      if (event.target.files.length > 0) {
+        this.image = event.target.files[0];
+      }
+    },
     createRecipe: function() {
-      var params = {
-        title: this.title,
-        ingredients: this.ingredients,
-        directions: this.directions,
-        image_url: this.imageUrl,
-        prep_time: this.prepTime
-      };
+      var formData = new FormData();
+      formData.append("title", this.title);
+      formData.append("ingredients", this.ingredients);
+      formData.append("directions", this.directions);
+      formData.append("image", this.image);
+      formData.append("prep_time", this.prepTime);
       axios
-        .post("/api/recipes", params)
-        .then(response => {
-          this.$parent.flashMessage = "Recipe successfully created!"
+        .post("/api/recipes", formData)
+        .then((response) => {
+          this.$parent.flashMessage = "Recipe successfully created!";
           this.$router.push("/recipes");
         })
-        .catch(error => {
+        .catch((error) => {
           this.errors = error.response.data.errors;
         });
-    }
-  }
+    },
+  },
 };
 </script>
